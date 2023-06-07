09aa35074edfe2ffba335677ea59613fe817fb1f
felix1805 <felix1805@gmail.com>
2023-06-07 15:57:19 -0400

    installed dependencies, created postgreSQL database

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import ListHeader from './components/ListHeader';
 
 const App = () => {
   return (
-    <div>
-   
+    <div className="app">
+      <ListHeader listName={"To Do List"} />
     </div>
   );
 }
