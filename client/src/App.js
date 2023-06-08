0ee441c9ceb89a7dedefc720cef1697a7ad37dd4
felix1805 <felix1805@gmail.com>
2023-06-08 16:31:44 -0400

    added buttons to ListItem.js, started adding functionality to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import ListHeader from './components/ListHeader';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import ListItem from './components/ListItem';
 
 const App = () => {
+  const userEmail = 'felix@test.com'
+  const [tasks, setTasks] = useState(null);
 
   const getData = async () => {
-    const userEmail = 'felix@test.com'
     try {
       const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
       const json = await response.json();
-      console.log(json);
+      setTasks(json);
     }
     catch (err) {
       console.error(err);
@@ -17,11 +19,18 @@ const App = () => {
 
   useEffect(() => getData, []); 
 
+  console.log(tasks);
+
+  // Sort by date
+  const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date));
+
   return (
     <div className="app">
       <ListHeader listName={"To Do List"} />
+      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} />)}
     </div>
   );
 }
 
 export default App;
+
--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,7 +1,19 @@
-const ListItem = () => {
+import ProgressBar from './ProgressBar';
+import TickIcon from './TickIcon';
+
+const ListItem = ({task}) => {
   return (
-    <div>
-    </div>
+    <li className="list-item">
+      <div className="info-container">
+        <TickIcon/>
+      <p className="task-title">{task.title}</p>
+      <ProgressBar/>
+      </div>
+      <div className='button-container'>
+        <button className='edit'>EDIT</button>
+        <button className='delete'>DELETE</button>
+      </div>
+    </li>
   );
 }
 
