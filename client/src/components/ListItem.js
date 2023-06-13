4f65969c36e69384a197a0ce4b620c3da1df209f
felix1805 <felix1805@gmail.com>
2023-06-13 16:57:30 -0400

    added delete function for listitem posts

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -3,20 +3,32 @@ import TickIcon from './TickIcon';
 import { useState } from 'react';
 import Modal from './Modal';
 
-const ListItem = ({task, getData}) => {
+const ListItem = ({ task, getData }) => {
   const [showModal, setShowModal] = useState(false)
+  const deleteItem = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${task.id}`, {
+        method: 'DELETE'
+      })
+      if (response.status === 200) {
+        getData()
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
   return (
     <li className="list-item">
       <div className="info-container">
-        <TickIcon/>
-      <p className="task-title">{task.title}</p>
-      <ProgressBar/>
+        <TickIcon />
+        <p className="task-title">{task.title}</p>
+        <ProgressBar />
       </div>
       <div className='button-container'>
         <button className='Edit' onClick={() => setShowModal(true)}>EDIT</button>
-        <button className='delete'>DELETE</button>
+        <button className='delete' onClick={deleteItem}>DELETE</button>
       </div>
-      {showModal && <Modal mode={'Edit'} setShowModal={setShowModal} getData={getData} task={task}/>}
+      {showModal && <Modal mode={'Edit'} setShowModal={setShowModal} getData={getData} task={task} />}
     </li>
   );
 }
--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ app.get('/todos/:userEmail', async (req, res) => {
   try {
     const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
     res.json(todos.rows);
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
   }
 });
 
@@ -31,7 +31,7 @@ app.post('/todos', async (req, res) => {
     const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)`,
       [id, user_email, title, progress, date])
     res.json(newToDo)
-  } catch {
+  } catch (err) {
     console.error(err)
   }
 })
@@ -43,6 +43,18 @@ app.put('/todos/:id', async (req, res) => {
   try {
     const editToDo = await pool.query('UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;', [user_email, title, progress, date, id])
     res.json(editToDo)
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+// delete a TODO element
+
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const deleteToDo = await pool.query('DELETE FROM todos WHERE id = $1;', [id])
+    res.json(deleteToDo)
   } catch (err) {
     console.error(err)
   }
